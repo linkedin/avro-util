a20fd47a4c94cdc54719b0061b8db36fad0af64c
Praveen Krishna <pravk03@gmail.com>
2020-04-01 15:14:11 -0700

    Fast Datum Reader/Writer Util with WeakIdentityHashMap (#37)
    * Fast Datum Reader/Writer Util with WeakIdentityHashMap
    
    * Wrapper created for ConcurrentHashMap with perf improvements . FastAvroConcurrentHashMap
    
    * Changed datum reader/writer to generic types
    
    Co-authored-by: prkrishn <prkrishn@linkedin.com>

---
+++ avro-fastserde/src/test/java/com/linkedin/avro/fastserde/FastDatumReaderWriterUtilTest.java
@@ -0,0 +1,66 @@
+package com.linkedin.avro.fastserde;
+
+import org.apache.avro.Schema;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class FastDatumReaderWriterUtilTest {
+
+  @Test (groups = "serializationTest")
+  public void testIsSupportedForFastGenericDatumWriter() {
+    Schema testSchema = Schema.parse("{\"type\": \"record\", \"name\": \"test_record\", \"fields\":[]}");
+    FastGenericDatumWriter fastWriter = FastDatumReaderWriterUtil.getFastGenericDatumWriter(testSchema);
+    Assert.assertNotNull(fastWriter);
+    FastGenericDatumWriter newFastWriter = FastDatumReaderWriterUtil.getFastGenericDatumWriter(testSchema);
+    Assert.assertSame(fastWriter, newFastWriter);
+  }
+
+  @Test (groups = "deserializationTest")
+  public void testIsSupportedForFastGenericDatumReader() {
+    Schema testWriterSchema = Schema.parse("{\"type\": \"record\", \"name\": \"test_record\", \"fields\":[]}");
+    Schema testReaderSchema = Schema.parse("{\"type\": \"record\", \"name\": \"test_record\", \"fields\":[]}");
+    FastGenericDatumReader fastReader = FastDatumReaderWriterUtil.getFastGenericDatumReader(testWriterSchema, testReaderSchema);
+    Assert.assertNotNull(fastReader);
+    FastGenericDatumReader newFastReader = FastDatumReaderWriterUtil.getFastGenericDatumReader(testWriterSchema, testReaderSchema);
+    Assert.assertSame(fastReader, newFastReader);
+  }
+
+  @Test (groups = "deserializationTest")
+  public void testIsSupportedForFastGenericDatumReaderWithSameReaderWriterSchema() {
+    Schema testSchema = Schema.parse("{\"type\": \"record\", \"name\": \"test_record\", \"fields\":[]}");
+    FastGenericDatumReader fastReader = FastDatumReaderWriterUtil.getFastGenericDatumReader(testSchema);
+    Assert.assertNotNull(fastReader);
+    FastGenericDatumReader newFastReader = FastDatumReaderWriterUtil.getFastGenericDatumReader(testSchema);
+    Assert.assertSame(fastReader, newFastReader);
+  }
+
+  @Test (groups = "serializationTest")
+  public void testIsSupportedForFastSpecificDatumWriter() {
+    Schema testSchema = Schema.parse("{\"type\": \"record\", \"name\": \"test_record\", \"fields\":[]}");
+    FastSpecificDatumWriter fastWriter = FastDatumReaderWriterUtil.getFastSpecificDatumWriter(testSchema);
+    Assert.assertNotNull(fastWriter);
+    FastSpecificDatumWriter newFastWriter = FastDatumReaderWriterUtil.getFastSpecificDatumWriter(testSchema);
+    Assert.assertSame(fastWriter, newFastWriter);
+  }
+
+  @Test (groups = "deserializationTest")
+  public void testIsSupportedForFastSpecificDatumReader() {
+    Schema testWriterSchema = Schema.parse("{\"type\": \"record\", \"name\": \"test_record\", \"fields\":[]}");
+    Schema testReaderSchema = Schema.parse("{\"type\": \"record\", \"name\": \"test_record\", \"fields\":[]}");
+    FastSpecificDatumReader fastReader = FastDatumReaderWriterUtil.getFastSpecificDatumReader(testWriterSchema, testReaderSchema);
+    Assert.assertNotNull(fastReader);
+    FastSpecificDatumReader newFastReader = FastDatumReaderWriterUtil.getFastSpecificDatumReader(testWriterSchema, testReaderSchema);
+    Assert.assertSame(fastReader, newFastReader);
+  }
+
+  @Test (groups = "deserializationTest")
+  public void testIsSupportedForFastSpecificDatumReaderWithSameReaderWriterSchema() {
+    Schema testSchema = Schema.parse("{\"type\": \"record\", \"name\": \"test_record\", \"fields\":[]}");
+    FastSpecificDatumReader fastReader = FastDatumReaderWriterUtil.getFastSpecificDatumReader(testSchema);
+    Assert.assertNotNull(fastReader);
+    FastSpecificDatumReader newFastReader = FastDatumReaderWriterUtil.getFastSpecificDatumReader(testSchema);
+    Assert.assertSame(fastReader, newFastReader);
+  }
+
+}
