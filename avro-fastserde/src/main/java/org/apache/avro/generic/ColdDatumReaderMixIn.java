00479d8f290110b0c7fee75c5591c34467f2c978
Felix GV <fgv@linkedin.com>
2020-06-01 15:53:49 -0700

    New Primitive List APIs
    Also added support for the new APIs in the ColdDatumReaderMixIn.
    
    Support after warm up is not there yet.

--- avro-fastserde/src/main/java/org/apache/avro/generic/ColdDatumReaderMixIn.java
+++ avro-fastserde/src/main/java/org/apache/avro/generic/ColdDatumReaderMixIn.java
@@ -1,6 +1,10 @@
 package org.apache.avro.generic;
 
+import com.linkedin.avro.fastserde.coldstart.ColdPrimitiveBooleanList;
+import com.linkedin.avro.fastserde.coldstart.ColdPrimitiveDoubleList;
 import com.linkedin.avro.fastserde.coldstart.ColdPrimitiveFloatList;
+import com.linkedin.avro.fastserde.coldstart.ColdPrimitiveIntList;
+import com.linkedin.avro.fastserde.coldstart.ColdPrimitiveLongList;
 import java.util.Collection;
 import org.apache.avro.Schema;
 
@@ -11,16 +15,36 @@ import org.apache.avro.Schema;
 public interface ColdDatumReaderMixIn {
   default Object newArray(Object old, int size, Schema schema, NewArrayFunction fallBackFunction) {
     switch (schema.getElementType().getType()) {
+      case BOOLEAN:
+        if (null == old || !(old instanceof ColdPrimitiveBooleanList)) {
+          old = new ColdPrimitiveBooleanList(size);
+        }
+        break;
+      case DOUBLE:
+        if (null == old || !(old instanceof ColdPrimitiveDoubleList)) {
+          old = new ColdPrimitiveDoubleList(size);
+        }
+        break;
       case FLOAT:
         if (null == old || !(old instanceof ColdPrimitiveFloatList)) {
-          return new ColdPrimitiveFloatList(size);
+          old = new ColdPrimitiveFloatList(size);
         }
-        ((Collection) old).clear();
-        return old;
-      // TODO: Add more cases when we support more primitive array types
+        break;
+      case INT:
+        if (null == old || !(old instanceof ColdPrimitiveIntList)) {
+          old = new ColdPrimitiveIntList(size);
+        }
+        break;
+      case LONG:
+        if (null == old || !(old instanceof ColdPrimitiveLongList)) {
+          old = new ColdPrimitiveLongList(size);
+        }
+        break;
       default:
         return fallBackFunction.newArray(old, size, schema);
     }
+    ((Collection) old).clear();
+    return old;
   }
 
   interface NewArrayFunction {
