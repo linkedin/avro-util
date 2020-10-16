c52235837db3954fd1c2a5671e0ccb4e5ed9f3ac
Felix GV <fgv@linkedin.com>
2020-10-16 13:42:55 -0700

    Code-gen for PR #101
    Generated code for Avro 1.5, 1.6, 1.9 and 1.10

---
+++ avro-fastserde/src/codegen/java/com/linkedin/avro/fastserde/generated/deserialization/AVRO_1_5/recordName_GenericDeserializer_6897301803194779359_6897301803194779359.java
@@ -0,0 +1,58 @@
+
+package com.linkedin.avro.fastserde.generated.deserialization.AVRO_1_5;
+
+import java.io.IOException;
+import com.linkedin.avro.fastserde.FastDeserializer;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.IndexedRecord;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.util.Utf8;
+
+public class recordName_GenericDeserializer_6897301803194779359_6897301803194779359
+    implements FastDeserializer<IndexedRecord>
+{
+
+    private final Schema readerSchema;
+    private final Schema unionField0;
+
+    public recordName_GenericDeserializer_6897301803194779359_6897301803194779359(Schema readerSchema) {
+        this.readerSchema = readerSchema;
+        this.unionField0 = readerSchema.getField("unionField").schema();
+    }
+
+    public IndexedRecord deserialize(IndexedRecord reuse, Decoder decoder)
+        throws IOException
+    {
+        return deserializerecordName0((reuse), (decoder));
+    }
+
+    public IndexedRecord deserializerecordName0(Object reuse, Decoder decoder)
+        throws IOException
+    {
+        IndexedRecord recordName;
+        if ((((reuse)!= null)&&((reuse) instanceof IndexedRecord))&&(((IndexedRecord)(reuse)).getSchema() == readerSchema)) {
+            recordName = ((IndexedRecord)(reuse));
+        } else {
+            recordName = new org.apache.avro.generic.GenericData.Record(readerSchema);
+        }
+        Object oldString0 = recordName.get(0);
+        if (oldString0 instanceof Utf8) {
+            recordName.put(0, (decoder).readString(((Utf8) oldString0)));
+        } else {
+            recordName.put(0, (decoder).readString(null));
+        }
+        int unionIndex0 = (decoder.readIndex());
+        switch (unionIndex0) {
+            case  0 :
+                decoder.readNull();
+                break;
+            case  1 :
+                recordName.put(1, deserializerecordName0(recordName.get(1), (decoder)));
+                break;
+            default:
+                throw new RuntimeException(("Illegal union index for 'unionField': "+ unionIndex0));
+        }
+        return recordName;
+    }
+
+}
---
+++ avro-fastserde/src/codegen/java/com/linkedin/avro/fastserde/generated/deserialization/AVRO_1_6/FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion_GenericDeserializer_4313387729290221819_4313387729290221819.java
@@ -0,0 +1,99 @@
+
+package com.linkedin.avro.fastserde.generated.deserialization.AVRO_1_6;
+
+import java.io.IOException;
+import com.linkedin.avro.fastserde.FastDeserializer;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.IndexedRecord;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.util.Utf8;
+
+public class FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion_GenericDeserializer_4313387729290221819_4313387729290221819
+    implements FastDeserializer<IndexedRecord>
+{
+
+    private final Schema readerSchema;
+    private final Schema union0;
+    private final Schema unionOptionSchema0;
+    private final Schema subField0;
+
+    public FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion_GenericDeserializer_4313387729290221819_4313387729290221819(Schema readerSchema) {
+        this.readerSchema = readerSchema;
+        this.union0 = readerSchema.getField("union").schema();
+        this.unionOptionSchema0 = union0 .getTypes().get(1);
+        this.subField0 = unionOptionSchema0 .getField("subField").schema();
+    }
+
+    public IndexedRecord deserialize(IndexedRecord reuse, Decoder decoder)
+        throws IOException
+    {
+        return deserializeFastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion0((reuse), (decoder));
+    }
+
+    public IndexedRecord deserializeFastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion0(Object reuse, Decoder decoder)
+        throws IOException
+    {
+        IndexedRecord FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion;
+        if ((((reuse)!= null)&&((reuse) instanceof IndexedRecord))&&(((IndexedRecord)(reuse)).getSchema() == readerSchema)) {
+            FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion = ((IndexedRecord)(reuse));
+        } else {
+            FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion = new org.apache.avro.generic.GenericData.Record(readerSchema);
+        }
+        int unionIndex0 = (decoder.readIndex());
+        switch (unionIndex0) {
+            case  0 :
+                decoder.readNull();
+                break;
+            case  1 :
+                FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion.put(0, deserializesubRecord0(FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion.get(0), (decoder)));
+                break;
+            case  2 :
+            {
+                Object oldString1 = FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion.get(0);
+                if (oldString1 instanceof Utf8) {
+                    FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion.put(0, (decoder).readString(((Utf8) oldString1)));
+                } else {
+                    FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion.put(0, (decoder).readString(null));
+                }
+                break;
+            }
+            case  3 :
+                FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion.put(0, (decoder.readInt()));
+                break;
+            default:
+                throw new RuntimeException(("Illegal union index for 'union': "+ unionIndex0));
+        }
+        return FastGenericDeserializerGeneratorTest_shouldReadMultipleChoiceUnion;
+    }
+
+    public IndexedRecord deserializesubRecord0(Object reuse, Decoder decoder)
+        throws IOException
+    {
+        IndexedRecord subRecord;
+        if ((((reuse)!= null)&&((reuse) instanceof IndexedRecord))&&(((IndexedRecord)(reuse)).getSchema() == unionOptionSchema0)) {
+            subRecord = ((IndexedRecord)(reuse));
+        } else {
+            subRecord = new org.apache.avro.generic.GenericData.Record(unionOptionSchema0);
+        }
+        int unionIndex1 = (decoder.readIndex());
+        switch (unionIndex1) {
+            case  0 :
+                decoder.readNull();
+                break;
+            case  1 :
+            {
+                Object oldString0 = subRecord.get(0);
+                if (oldString0 instanceof Utf8) {
+                    subRecord.put(0, (decoder).readString(((Utf8) oldString0)));
+                } else {
+                    subRecord.put(0, (decoder).readString(null));
+                }
+                break;
+            }
+            default:
+                throw new RuntimeException(("Illegal union index for 'subField': "+ unionIndex1));
+        }
+        return subRecord;
+    }
+
+}
