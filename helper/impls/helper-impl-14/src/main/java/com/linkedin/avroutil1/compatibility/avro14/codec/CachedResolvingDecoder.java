e21bb952eb99cb8dfb2236e8c5111ce4121cc0bc
guawang <guawang@linkedin.com>
2020-11-06 16:44:29 -0800

    Add CachedResolvingDecoder and related classes

---
+++ helper/impls/helper-impl-14/src/main/java/com/linkedin/avroutil1/compatibility/avro14/codec/CachedResolvingDecoder.java
@@ -0,0 +1,75 @@
+package com.linkedin.avroutil1.compatibility.avro14.codec;
+
+import com.linkedin.avroutil1.compatibility.avro14.parsing.CachedResolvingGrammarGenerator;
+import com.linkedin.avroutil1.compatibility.avro14.parsing.Symbol;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.avro.Schema;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.Decoder;
+
+
+public class CachedResolvingDecoder extends ResolvingDecoder {
+  private static final ConcurrentHashMap<Schema, ConcurrentHashMap<Schema, Symbol>>
+      SYMBOL_CACHE = new ConcurrentHashMap<>();
+
+  public CachedResolvingDecoder(Schema writer, Schema reader, Decoder in) throws IOException {
+    this(resolve(writer, reader), in);
+  }
+
+  /**
+   * Constructs a <tt>CachedResolvingDecoder</tt> using the given resolver.
+   * The resolver must have been returned by a previous call to
+   * {@link #resolve(Schema, Schema)}.
+   * @param resolver  The resolver to use.
+   * @param in  The underlying decoder.
+   * @throws IOException
+   */
+  public CachedResolvingDecoder(Object resolver, Decoder in)
+      throws IOException {
+    super(resolver, in);
+  }
+  /*skips the symbol and returns the size of the string to copy*/
+  public int readStringSize() throws IOException {
+    parser.advance(Symbol.STRING);
+    return in.readInt();
+  }
+  /*skips the symbol and returns the size of the bytes to copy*/
+  public int readBytesSize() throws IOException {
+    parser.advance(Symbol.BYTES);
+    return in.readInt();
+  }
+  /*returns the string data*/
+  public void readStringData(byte[] bytes, int start, int len) throws IOException {
+    in.readFixed(bytes, start, len);
+  }
+  /*returns the bytes data*/
+  public void readBytesData(byte[] bytes, int start, int len) throws IOException {
+    in.readFixed(bytes, start, len);
+  }
+
+  public boolean isBinaryDecoder() {
+    return this.in instanceof BinaryDecoder;
+  }
+  /**
+   * Produces an opaque resolver that can be used to construct a new
+   * {@link ResolvingDecoder (Object, Decoder)}. The
+   * returned Object is immutable and hence can be simultaneously used
+   * in many ResolvingDecoders. This method is reasonably expensive, the
+   * users are encouraged to cache the result.
+   *
+   * @param writer  The writer's schema.
+   * @param reader  The reader's schema.
+   * @return  The opaque reolver.
+   * @throws IOException
+   */
+  public static Object resolve(Schema writer, Schema reader) throws IOException {
+    ConcurrentHashMap<Schema,Symbol> cache = SYMBOL_CACHE.computeIfAbsent(writer, k -> new ConcurrentHashMap<>());
+    Symbol resolver = cache.get(reader);
+    if (resolver == null) {
+      resolver =  new CachedResolvingGrammarGenerator().generate(writer, reader);
+      cache.put(reader, resolver);
+    }
+    return resolver;
+  }
+}
---
+++ helper/impls/helper-impl-14/src/main/java/com/linkedin/avroutil1/compatibility/avro14/parsing/CachedSymbol.java
@@ -0,0 +1,44 @@
+package com.linkedin.avroutil1.compatibility.avro14.parsing;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+
+public class CachedSymbol extends Symbol {
+  protected CachedSymbol(Kind kind) {
+    this(kind, null);
+  }
+
+  protected CachedSymbol(Kind kind, Symbol[] production) {
+    super(kind,production);
+  }
+
+  static final Field TRAILING_FIELD;
+  static {
+    try {
+      TRAILING_FIELD = ImplicitAction.class.getDeclaredField("isTrailing");
+    } catch (Throwable ex) {
+      throw new Error(ex);
+    }
+    TRAILING_FIELD.setAccessible(true);
+  }
+
+  public static class CachedSkipAction extends SkipAction {
+    public CachedSkipAction(Symbol symToSkip) {
+      super(symToSkip);
+      try
+      {
+        TRAILING_FIELD.setBoolean(this, true);
+      } catch (IllegalAccessException e)
+      {
+        throw new Error(e);
+      }
+    }
+    @Override
+    public SkipAction flatten(Map map,
+        Map map2) {
+      return new CachedSkipAction(symToSkip.flatten(map, map2));
+    }
+  }
+}
+
