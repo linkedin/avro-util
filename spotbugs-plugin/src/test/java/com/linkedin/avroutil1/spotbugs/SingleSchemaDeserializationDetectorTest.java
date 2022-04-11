a21320db6fa8193e5172e30d4bd63e5c530e2cb6
Uttam Kumar <ukumar@linkedin.com>
2022-04-11 12:02:17 -0700

    Stop throwing SINGLE_SCHEMA_DESERIALIZATION if class is child of GenericDatumReader (#311)
    * Stop throwing SINGLE_SCHEMA_DESERIALIZATION if class is child of GenericDatumReader
    
    Co-authored-by: Uttam Kumar <ukumar@ukumar-mn2.linkedin.biz>

--- spotbugs-plugin/src/test/java/com/linkedin/avroutil1/spotbugs/SingleSchemaDeserializationDetectorTest.java
+++ spotbugs-plugin/src/test/java/com/linkedin/avroutil1/spotbugs/SingleSchemaDeserializationDetectorTest.java
@@ -31,7 +31,18 @@ public class SingleSchemaDeserializationDetectorTest {
         BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
                 .bugType("SINGLE_SCHEMA_DESERIALIZATION")
                 .inMethod("singleSchemaDecoding").build();
-        MatcherAssert.assertThat(bugCollection, containsExactly(2, bugTypeMatcher));
+        MatcherAssert.assertThat(bugCollection, containsExactly(1, bugTypeMatcher));
+    }
+
+    @Test
+    public void testFalsePositive() {
+        Path path = Paths.get("build/classes/java/test", DatumReaderClass.class.getName().replace('.', '/') + ".class");
+        BugCollection bugCollection = spotbugs.performAnalysis(path);
+
+        BugInstanceMatcher bugTypeMatcher = new BugInstanceMatcherBuilder()
+            .bugType("SINGLE_SCHEMA_DESERIALIZATION")
+            .inMethod("singleSchemaDecoding").build();
+        MatcherAssert.assertThat(bugCollection, containsExactly(0, bugTypeMatcher));
     }
 
     @Test
