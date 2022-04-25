51d1c46783cdc2b522a3d04a81bf654216835290
Radai Rosenblatt <radai.rosenblatt@gmail.com>
2022-04-25 16:28:35 -0700

    add RandomRecordGenerator handling for self-referencing schemas (#321)
    errsolves #319
    
    Co-authored-by: Radai Rosenblatt <rrosenbl@rrosenbl-mn2.linkedin.biz>

--- helper/tests/helper-tests-allavro/src/test/java/com/linkedin/avroutil1/compatibility/RandomRecordGeneratorTest.java
+++ helper/tests/helper-tests-allavro/src/test/java/com/linkedin/avroutil1/compatibility/RandomRecordGeneratorTest.java
@@ -6,6 +6,8 @@
 
 package com.linkedin.avroutil1.compatibility;
 
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -24,11 +26,27 @@ public class RandomRecordGeneratorTest {
     testSpecific(under111.RecordWithReservedFields.class);
   }
 
+  @Test
+  public void testRecordsWithSelfReferences() throws Exception {
+    //default config has a 20% chance of populating a reference to an already-visited schema
+    //but these should definitely not throw stack overflows.
+    testSpecific(under14.RecordWithSelfReferences.class);
+    testGeneric(under14.RecordWithSelfReferences.SCHEMA$);
+  }
+
   private void testSpecific(Class<?> srClass) throws Exception {
     RandomRecordGenerator gen = new RandomRecordGenerator();
     RecordGenerationConfig config = RecordGenerationConfig.newConfig().withAvoidNulls(true);
     Object srInstance = gen.randomSpecific(srClass, config);
     Assert.assertNotNull(srInstance);
     Assert.assertTrue(srClass.isInstance(srInstance));
+  }
+
+  private void testGeneric(Schema schema) throws Exception {
+    RandomRecordGenerator gen = new RandomRecordGenerator();
+    RecordGenerationConfig config = RecordGenerationConfig.newConfig().withAvoidNulls(true);
+    Object grInstance = gen.randomGeneric(schema, config);
+    Assert.assertNotNull(grInstance);
+    Assert.assertTrue(grInstance instanceof GenericRecord);
   }
 }
