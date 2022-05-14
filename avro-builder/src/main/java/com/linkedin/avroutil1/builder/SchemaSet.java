a6cce1a11076b505b966a72b6943c2fcd1792cb6
Sourav Maji <majisourav@gmail.com>
2022-05-13 17:28:49 -0700

    Moved package from avro to avroutil1

---
+++ avro-builder/src/main/java/com/linkedin/avroutil1/builder/SchemaSet.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 LinkedIn Corp.
+ * Licensed under the BSD 2-Clause License (the "License").
+ * See License in the project root for license information.
+ */
+
+package com.linkedin.avroutil1.builder;
+
+
+import java.util.List;
+
+import org.apache.avro.Schema;
+
+
+/**
+ * provides a set of schemas
+ */
+public interface SchemaSet {
+
+  /**
+   * @return how many schemas in the set
+   */
+  int size();
+
+  /**
+   * @param name FQCN of a schema (so "com.acme.Foo")
+   * @return the schema, if this set has it
+   */
+  Schema getByName(String name);
+
+  /**
+   * @return all schemas in this set
+   */
+  List<Schema> getAll();
+
+  /**
+   * @param schema to be added
+   */
+  void add(Schema schema);
+}
