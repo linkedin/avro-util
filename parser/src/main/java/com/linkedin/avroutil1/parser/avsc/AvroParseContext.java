ea93a49b1a1fc6579d42524b8dbc465d3808056b
David Grossman <dgrossman@linkedin.com>
2022-10-05 11:41:19 -0400

    move CP import logic to the end of parsing

--- parser/src/main/java/com/linkedin/avroutil1/parser/avsc/AvroParseContext.java
+++ parser/src/main/java/com/linkedin/avroutil1/parser/avsc/AvroParseContext.java
@@ -37,16 +37,16 @@ public class AvroParseContext {
     /**
      * all named (record, enum, fixed) schemas defined in any importable source file
      */
-    private Map<String, AvscParseResult> importableNamedSchemas = null;
+    private Map<String, AvscParseResult> importableNamedSchemas = new HashMap<>(1);
     /**
      * all named (record, enum, fixed) schemas defined in any source file
      */
-    private Map<String, AvscParseResult> allNamedSchemas = null;
+    private Map<String, AvscParseResult> allNamedSchemas = new HashMap<>(1);
     /**
      * these are the top-level (outermost) schemas defined, one per file
      */
-    private Set<AvroSchema> topLevelSchemas = null;
-    private Map<String, List<AvscParseResult>> duplicates = null;
+    private Set<AvroSchema> topLevelSchemas = new HashSet<>(1);
+    private Map<String, List<AvscParseResult>> duplicates = new HashMap<>(1);
     /**
      * remaining unresolved references after all references across files that
      * are part of this context have been handled.
@@ -70,54 +70,46 @@ public class AvroParseContext {
         }
         assertMutable();
         individualResults.add(new AvscStandaloneResult(singleResult, isImportable));
+
+        //add to an index of FQCNs (also find dups)
+        Throwable error = singleResult.getParseError();
+        if (error != null) {
+            //don't touch files with outright failures
+            //TODO - add an issue here
+            return;
+        }
+        Map<String, AvroNamedSchema> namedInFile = singleResult.getDefinedNamedSchemas();
+        namedInFile.forEach((fqcn, schema) -> {
+            AvscParseResult prior = allNamedSchemas.putIfAbsent(fqcn, singleResult);
+            if (prior != null) {
+                //TODO - find dups in aliases as well ?
+                //this is a dup
+                duplicates.compute(fqcn, (k, dups) -> {
+                    if (dups == null) {
+                        dups = new ArrayList<>(2);
+                        dups.add(prior);
+                    }
+                    dups.add(singleResult);
+                    return dups;
+                });
+                //TODO - add context-level issues for dups
+            }
+            if (isImportable) {
+                //dont care about return value since we handled dups above
+                importableNamedSchemas.putIfAbsent(fqcn, singleResult);
+            }
+        });
+        topLevelSchemas.add(singleResult.getTopLevelSchema());
     }
 
     /**
      * expected to be called once after all individual file parsing results have been added.
-     * this method resolves any cross-file references in schemas and populates
-     * the index datastructures.
+     * this method resolves any cross-file references in schemas.
      * no file parsing results can be added to this context once this method is called.
      */
     public void resolveReferences() {
         assertMutable();
         sealed = true;
-
-        //build up an index of FQCNs (also find dups)
-        importableNamedSchemas = new HashMap<>(individualResults.size());
-        allNamedSchemas = new HashMap<>(individualResults.size());
-        topLevelSchemas = new HashSet<>(individualResults.size());
-        duplicates = new HashMap<>(1);
-        for (AvscStandaloneResult fileResult : individualResults) {
-            AvscParseResult parseResult = fileResult.parseResult;
-            Throwable error = parseResult.getParseError();
-            if (error != null) {
-                //don't touch files with outright failures
-                //TODO - add an issue here
-                continue;
-            }
-            Map<String, AvroNamedSchema> namedInFile = parseResult.getDefinedNamedSchemas();
-            namedInFile.forEach((fqcn, schema) -> {
-                AvscParseResult prior = allNamedSchemas.putIfAbsent(fqcn, parseResult);
-                if (prior != null) {
-                    //TODO - find dups in aliases as well ?
-                    //this is a dup
-                    duplicates.compute(fqcn, (k, dups) -> {
-                        if (dups == null) {
-                            dups = new ArrayList<>(2);
-                            dups.add(prior);
-                        }
-                        dups.add(parseResult);
-                        return dups;
-                    });
-                }
-                if (fileResult.isImportable) {
-                    //dont care about return value since we handled dups above
-                    importableNamedSchemas.putIfAbsent(fqcn, parseResult);
-                }
-            });
-            topLevelSchemas.add(parseResult.getTopLevelSchema());
-        }
-        //TODO - add context-level issues for dups
 
         //resolve any unresolved references in individual file results from other files
         externalReferences = new ArrayList<>();
@@ -139,8 +131,9 @@ public class AvroParseContext {
                 if (inheritedNameResolution != null) {
                     ref.setResolvedTo(inheritedNameResolution.getDefinedNamedSchemas().get(inheritedName));
                     if (simpleNameResolution != null) {
-                        String msg = "ERROR: Two different schemas found for reference " + simpleName
-                            + " with inherited name " + inheritedName + ". Only one should exist.";
+                        String msg =
+                            "ERROR: Two different schemas found for reference " + simpleName + " with inherited name "
+                                + inheritedName + ". Only one should exist.";
                         singleFile.parseResult.addIssue(new AvscIssue(ref.getCodeLocation(), IssueSeverity.WARNING, msg,
                             new IllegalStateException(msg)));
                     }
