34d8caeb97d1a212659a5bd36d4164ddf761f171
Aliaksei Dubrouski <adubrouski@linkedin.com>
2023-04-06 18:31:01 -0700

    Adding unit test to cover this scenario

---
+++ fastserde/avro-fastserde-tests-common/src/test/java/com/linkedin/avro/fastserde/BufferBackedPrimitiveFloatListTest.java
@@ -0,0 +1,39 @@
+package com.linkedin.avro.fastserde;
+
+import com.linkedin.avro.fastserde.primitive.PrimitiveFloatArrayList;
+import com.linkedin.avro.fastserde.primitive.PrimitiveLongArrayList;
+import com.linkedin.avroutil1.compatibility.AvroCompatibilityHelper;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.io.Encoder;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BufferBackedPrimitiveFloatListTest {
+  @Test
+  public void testPrimitiveFloatArrayAdd() throws IOException {
+    Schema arraySchema = Schema.createArray(Schema.create(Schema.Type.FLOAT));
+    List<Float> testFloatArray = Arrays.asList(1.0f, 2.0f, 3.0f);
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    Encoder binaryEncoder = AvroCompatibilityHelper.newBinaryEncoder(baos, true, null);
+    try {
+      GenericDatumWriter<Object> writer = new GenericDatumWriter<>(arraySchema);
+      writer.write(testFloatArray, binaryEncoder);
+      binaryEncoder.flush();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    BinaryDecoder decoder = DecoderFactory.defaultFactory().createBinaryDecoder(baos.toByteArray(), null);
+    BufferBackedPrimitiveFloatList list = (BufferBackedPrimitiveFloatList) BufferBackedPrimitiveFloatList
+        .readPrimitiveFloatArray(new Object(), decoder);
+    Assert.assertEquals(list.remove(2), 3.0f);
+    }
+}
