5df0df49b46fcf103a4afa30dddbf03edd41a58d
Andy Sautins <asautins@linkedin.com>
2023-04-14 18:51:07 -0600

    Use helper functions so test builds on all versions

--- fastserde/avro-fastserde-tests-common/src/test/java/com/linkedin/avro/fastserde/FastNestedArrayTest.java
+++ fastserde/avro-fastserde-tests-common/src/test/java/com/linkedin/avro/fastserde/FastNestedArrayTest.java
@@ -3,16 +3,10 @@ package com.linkedin.avro.fastserde;
 import com.linkedin.avro.fastserde.generated.avro.NestedArrayItem;
 import com.linkedin.avro.fastserde.generated.avro.NestedArrayTest;
 
-import com.linkedin.avroutil1.compatibility.AvroCompatibilityHelper;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import org.apache.avro.io.Decoder;
-import org.apache.avro.io.DecoderFactory;
-import org.apache.avro.io.Encoder;
-import org.apache.avro.specific.SpecificDatumWriter;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -26,27 +20,16 @@ public class FastNestedArrayTest {
 
     List<NestedArrayItem> items = new ArrayList<>();
     NestedArrayItem item = new NestedArrayItem();
-    item.setItemName("itemName");
-    items.add(item);
+    FastSerdeTestsSupport.setField(item, "itemName", "itemName");
+    FastSerdeTestsSupport.setField(nestedArrayTest, "NestedArrayItems", Collections.singletonList(items));
 
-    nestedArrayTest.setNestedArrayItems(Collections.singletonList(items));
-
-    SpecificDatumWriter<NestedArrayTest> writer =
-        new SpecificDatumWriter<>(NestedArrayTest.SCHEMA$);
-    ByteArrayOutputStream os = new ByteArrayOutputStream();
-    Encoder encoder =
-        AvroCompatibilityHelper.newJsonEncoder(NestedArrayTest.SCHEMA$, os, true);
-    writer.write(nestedArrayTest, encoder);
-    encoder.flush();
+    Decoder decoder = FastSerdeTestsSupport.specificDataAsDecoder(nestedArrayTest, NestedArrayTest.SCHEMA$);
 
     FastSpecificDatumReader<NestedArrayTest> fastSpecificDatumReader =
         new FastSpecificDatumReader<>(NestedArrayTest.SCHEMA$);
     FastDeserializer<NestedArrayTest> fastDeserializer =
         fastSpecificDatumReader.getFastDeserializer().get();
 
-    ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
-    Decoder decoder =
-        DecoderFactory.defaultFactory().jsonDecoder(NestedArrayTest.SCHEMA$, is);
     NestedArrayTest actual = fastDeserializer.deserialize(decoder);
 
     Assert.assertEquals(actual, nestedArrayTest);
