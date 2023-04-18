e490fee675285fabbbaf45ea93465f950df99238
Ajinkya Dande <adande@adande-mn1.linkedin.biz>
2023-04-17 20:20:03 -0400

    Fixing checkstyle errors

--- parser/src/main/java/com/linkedin/avroutil1/util/ConfigurableAvroSchemaComparator.java
+++ parser/src/main/java/com/linkedin/avroutil1/util/ConfigurableAvroSchemaComparator.java
@@ -58,6 +58,7 @@ public class ConfigurableAvroSchemaComparator {
     try {
       equals(a, b, config, seen, differences);
     } catch (IOException ignored) {
+      // ignored
     }
     return differences;
   }
