b93c49af809233ed1e0210b4994f02b8dc288660
Adam Peck <kcepmada@gmail.com>
2023-06-29 13:44:54 -0700

    Optimize enum field deserialization
    BUG=FI-13182
    Closes #302
    
    ClassValue lazily caches result of Enum#values for the JVM to avoid unnecessary memory overhead.
    
    odp download --event-id 1094224
    * com.linkedin.followfeed.partitioner.graph.ContentQualityClassification[].values()
      Total Allocation: 20.5G (3.58%)

--- avro-codegen/src/test/java/com/linkedin/avroutil1/codegen/SpecificRecordClassGeneratorTest.java
+++ avro-codegen/src/test/java/com/linkedin/avroutil1/codegen/SpecificRecordClassGeneratorTest.java
@@ -7,8 +7,7 @@
 package com.linkedin.avroutil1.codegen;
 
 import com.linkedin.avroutil1.compatibility.StringUtils;
-import com.linkedin.avroutil1.model.AvroEnumSchema;
-import com.linkedin.avroutil1.model.AvroFixedSchema;
+import com.linkedin.avroutil1.model.AvroNamedSchema;
 import com.linkedin.avroutil1.model.AvroRecordSchema;
 import com.linkedin.avroutil1.parser.avsc.AvscParseResult;
 import com.linkedin.avroutil1.parser.avsc.AvscParser;
@@ -22,176 +21,43 @@ import org.testng.annotations.Test;
 
 
 public class SpecificRecordClassGeneratorTest {
-
-  @Test
-  public void testSimpleEnum() throws Exception {
-    String avsc = TestUtil.load("schemas/SimpleEnum.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroEnumSchema enumSchema = (AvroEnumSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(enumSchema);
-    JavaFileObject javaFileObject = generator.generateSpecificClass(enumSchema,
-        SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-    CompilerHelper.assertCompiles(javaFileObject);
-
-  }
-
-  @Test
-  public void testHugeEnum() throws Exception {
-    String avsc = TestUtil.load("schemas/SimpleEnumWithHugeDoc.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroEnumSchema enumSchema = (AvroEnumSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(enumSchema);
-    JavaFileObject javaFileObject =
-        generator.generateSpecificClass(enumSchema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-    CompilerHelper.assertCompiles(javaFileObject);
-
-  }
-
-  @Test
-  public void testSimpleFixed() throws Exception {
-    String avsc = TestUtil.load("schemas/SimpleFixed.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroFixedSchema fixedSchema = (AvroFixedSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(fixedSchema);
-    JavaFileObject javaFileObject = generator.generateSpecificClass(fixedSchema,
-        SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-    CompilerHelper.assertCompiles(javaFileObject);
-
-  }
-
-  @Test
-  public void testSimpleFixedWithHugeDoc() throws Exception {
-    String avsc = TestUtil.load("schemas/SimpleFixedWithHugeDoc.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroFixedSchema fixedSchema = (AvroFixedSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(fixedSchema);
-    JavaFileObject javaFileObject =
-        generator.generateSpecificClass(fixedSchema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-    CompilerHelper.assertCompiles(javaFileObject);
-
-  }
-
   @DataProvider
-  Object[][] testRecordWithArrayOfRecordsProvider() {
+  Object[][] schemaPaths() {
     return new Object[][]{
-        {"schemas/ArrayOfStringRecord.avsc"},
-        {"schemas/ArrayOfRecords.avsc"},
-        {"schemas/TestCollections.avsc"}
+        {"schemas/ArrayOfRecords.avsc", false},
+        {"schemas/ArrayOfStringRecord.avsc", true},
+        {"schemas/BuilderTester.avsc", false},
+        {"schemas/DollarSignInDoc.avsc", true},
+        {"schemas/MoneyRange.avsc", false},
+        {"schemas/RecordDefault.avsc", false},
+        {"schemas/RecordWithRecordOfEnum.avsc", false},
+        {"schemas/RecordWithRecordOfRecord.avsc", false},
+        {"schemas/SimpleEnum.avsc", true},
+        {"schemas/SimpleEnumWithHugeDoc.avsc", true},
+        {"schemas/SimpleFixed.avsc", true},
+        {"schemas/SimpleFixedWithHugeDoc.avsc", true},
+        {"schemas/TestCollections.avsc", true},
+        {"schemas/TestProblematicRecord.avsc", true},
+        {"schemas/TestRecord.avsc", false}
     };
   }
 
-  @Test(dataProvider = "testRecordWithArrayOfRecordsProvider")
-  public void testRecordWithArrayOfRecords(String path) throws Exception {
+  @Test(dataProvider = "schemaPaths")
+  public void testSchema(String path, boolean checkCompiles) throws Exception {
     String avsc = TestUtil.load(path);
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
     AvscParser parser = new AvscParser();
     AvscParseResult result = parser.parse(avsc);
     Assert.assertNull(result.getParseError());
-    AvroRecordSchema recordSchema = (AvroRecordSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(recordSchema);
-    JavaFileObject javaFileObject =
-        generator.generateSpecificClass(recordSchema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-
-  }
-
-  @DataProvider
-  Object[][] testSpecificWithInternalClassesProvider() {
-    return new Object[][]{
-        {"schemas/RecordWithRecordOfEnum.avsc"},
-        {"schemas/RecordWithRecordOfRecord.avsc"},
-        {"schemas/TestRecord.avsc"}
-    };
-  }
-
-  @Test(dataProvider = "testSpecificWithInternalClassesProvider")
-  public void testSpecificWithInternalClasses(String filePath) throws Exception {
-    String avsc = TestUtil.load(filePath);
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroRecordSchema recordSchema = (AvroRecordSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(recordSchema);
-    JavaFileObject javaSourceFile = generator.generateSpecificClass(recordSchema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-  }
-
-  @Test
-  public void testSpecificWith$InDoc() throws Exception {
-    String avsc = TestUtil.load("schemas/DollarSignInDoc.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroRecordSchema recordSchema = (AvroRecordSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(recordSchema);
-    JavaFileObject javaFileObject =
-        generator.generateSpecificClass(recordSchema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-    CompilerHelper.assertCompiles(javaFileObject);
-  }
-
-  @Test
-  public void testSpecificRecordWithInternalDefinedTypeReuse() throws Exception {
-    String avsc = TestUtil.load("schemas/MoneyRange.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroRecordSchema recordSchema = (AvroRecordSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(recordSchema);
-    JavaFileObject javaFileObject =
-        generator.generateSpecificClass(recordSchema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-  }
-
-  @Test
-  public void testDefaultForRecord() throws Exception {
-    String avsc = TestUtil.load("schemas/RecordDefault.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroRecordSchema recordSchema = (AvroRecordSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(recordSchema);
-    JavaFileObject javaFileObject =
-        generator.generateSpecificClass(recordSchema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-  }
-
-  @Test
-  public void testRecord() throws Exception {
-    String avsc = TestUtil.load("schemas/BuilderTester.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroRecordSchema recordSchema = (AvroRecordSchema) result.getTopLevelSchema();
-    Assert.assertNotNull(recordSchema);
-    JavaFileObject javaFileObject =
-        generator.generateSpecificClass(recordSchema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-  }
-
-  @Test
-  public void testProblematicRecord() throws Exception {
-    String avsc = TestUtil.load("schemas/TestProblematicRecord.avsc");
-    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
-    AvscParser parser = new AvscParser();
-    AvscParseResult result = parser.parse(avsc);
-    Assert.assertNull(result.getParseError());
-    AvroRecordSchema schema  = (AvroRecordSchema) result.getTopLevelSchema();
+    AvroNamedSchema schema = (AvroNamedSchema) result.getTopLevelSchema();
     Assert.assertNotNull(schema);
+
+    SpecificRecordClassGenerator generator = new SpecificRecordClassGenerator();
     JavaFileObject javaFileObject =
         generator.generateSpecificClass(schema, SpecificRecordGenerationConfig.BROAD_COMPATIBILITY);
-    CompilerHelper.assertCompiles(javaFileObject);
+
+    if (checkCompiles) {
+      CompilerHelper.assertCompiles(javaFileObject);
+    }
   }
 
   @Test
