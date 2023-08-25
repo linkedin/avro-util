ecebef26fd70e4d0daedaf685f7fa3fc53a2aed0
David Grossman <dgrossman@linkedin.com>
2023-08-25 09:30:17 -0700

    AvroCodecUtil#serializeJson() - add method with `bool oneline` option (#510)

--- helper/helper/src/main/java/com/linkedin/avroutil1/compatibility/AvroCodecUtil.java
+++ helper/helper/src/main/java/com/linkedin/avroutil1/compatibility/AvroCodecUtil.java
@@ -48,9 +48,22 @@ public class AvroCodecUtil {
         return os.toByteArray();
     }
 
+   /**
+    * serializes an IndexedRecord to a json string in Avro's json encoding format (pretty-printed).
+    */
     public static String serializeJson(IndexedRecord record, AvroVersion format) throws IOException {
+        return serializeJson(record, format, false);
+    }
+
+   /**
+    * Serialize an IndexedRecord (Generic Record or Specific Record) to json string (in Avro's json encoding format).
+    * @param record the record to serialize
+    * @param format the version of avro to use
+    * @param oneline whether to output the json in one line or pretty printed.
+    */
+    public static String serializeJson(IndexedRecord record, AvroVersion format, boolean oneline) throws IOException {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        Encoder encoder = AvroCompatibilityHelper.newJsonEncoder(record.getSchema(), os, true, format);
+        Encoder encoder = AvroCompatibilityHelper.newJsonEncoder(record.getSchema(), os, !oneline, format);
         DatumWriter<IndexedRecord> writer = AvroCompatibilityHelper.isSpecificRecord(record) ?
                 new SpecificDatumWriter<>(record.getSchema())
                 : new GenericDatumWriter<>(record.getSchema());
