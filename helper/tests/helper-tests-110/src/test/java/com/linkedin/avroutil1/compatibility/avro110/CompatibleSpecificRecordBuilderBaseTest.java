4c6ec4a13bc6ff3a8a5b3a816dc3cf6e435d21c8
Uttam Kumar <ukumar@linkedin.com>
2023-09-06 19:34:53 -0700

    Adding UT to check validate in CompatibleSpecificRecordBuilderBase

---
+++ helper/tests/helper-tests-110/src/test/java/com/linkedin/avroutil1/compatibility/avro110/CompatibleSpecificRecordBuilderBaseTest.java
@@ -0,0 +1,28 @@
+package com.linkedin.avroutil1.compatibility.avro110;
+
+import com.linkedin.avroutil1.compatibility.CompatibleSpecificRecordBuilderBase;
+import java.util.Arrays;
+import org.apache.avro.Schema;
+import org.testng.annotations.Test;
+
+
+public class CompatibleSpecificRecordBuilderBaseTest {
+
+  @Test
+  public void testValidate() {
+    Schema.Field field = new Schema.Field("field1", Schema.create(Schema.Type.STRING), "field doc", "null");
+    Schema myRecord = Schema.createRecord(Arrays.asList(field));
+    new MyBuilderClass(myRecord).testValidate(field, null);
+  }
+
+  class MyBuilderClass extends CompatibleSpecificRecordBuilderBase {
+
+    protected MyBuilderClass(Schema schema) {
+      super(schema);
+    }
+
+    public void testValidate(Schema.Field f, Object o) {
+      validate(f, o);
+    }
+  }
+}
