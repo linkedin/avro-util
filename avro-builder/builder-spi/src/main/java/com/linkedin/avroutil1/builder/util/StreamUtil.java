8b490a15b8579b6941170e43ca5ee888d381254f
Karthik Ramgopal <karthikrg.bits@gmail.com>
2024-01-22 14:42:44 -0800

    Optimize StreamUtil by adding batching support (#543)
    * Optimize StreamUtil by adding batching support
    
    * Fix javadoc
    
    * Fix javadoc
    
    ---------
    
    Co-authored-by: Karthik Ramgopal <kramgopa@linkedin.com>

--- avro-builder/builder-spi/src/main/java/com/linkedin/avroutil1/builder/util/StreamUtil.java
+++ avro-builder/builder-spi/src/main/java/com/linkedin/avroutil1/builder/util/StreamUtil.java
@@ -6,14 +6,17 @@
 
 package com.linkedin.avroutil1.builder.util;
 
-import com.pivovarit.collectors.ParallelCollectors;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 
@@ -37,8 +40,8 @@ public final class StreamUtil {
   /**
    * A convenience {@link Collector} used for executing parallel computations on a custom {@link Executor}
    * and returning a {@link Stream} instance returning results as they arrive.
-   * <p>
-   * For the parallelism of 1, the stream is executed by the calling thread.
+   *
+   * <p>For the parallelism of 1, the stream is executed by the calling thread.</p>
    *
    * @param mapper      a transformation to be performed in parallel
    * @param parallelism the max parallelism level
@@ -48,6 +51,70 @@ public final class StreamUtil {
    * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel.
    */
   public static <T, R> Collector<T, ?, Stream<R>> toParallelStream(Function<T, R> mapper, int parallelism) {
-    return ParallelCollectors.parallelToStream(mapper, WORK_EXECUTOR, parallelism);
+    return toParallelStream(mapper, parallelism, 1);
+  }
+
+  /**
+   * A convenience {@link Collector} used for executing parallel computations on a custom {@link Executor}
+   * and returning a {@link Stream} instance returning results as they arrive.
+   *
+   * <p>For the parallelism of 1 or if the size of the elements is &lt;= batchSize, the stream is executed by the
+   * calling thread.</p>
+   *
+   * @param mapper      a transformation to be performed in parallel
+   * @param parallelism the max parallelism level
+   * @param batchSize   the size into which inputs should be batched before running the mapper.
+   * @param <T>         the type of the collected elements
+   * @param <R>         the result returned by {@code mapper}
+   *
+   * @return a {@code Collector} which collects all processed elements into a {@code Stream} in parallel.
+   */
+  public static <T, R> Collector<T, ?, Stream<R>> toParallelStream(Function<T, R> mapper, int parallelism,
+      int batchSize) {
+    if (parallelism <= 0 || batchSize <= 0) {
+      throw new IllegalArgumentException("Parallelism and batch size must be >= 1");
+    }
+
+    return Collectors.collectingAndThen(Collectors.toList(), list -> {
+      if (list.isEmpty()) {
+        return Stream.empty();
+      }
+
+      if (parallelism == 1 || list.size() <= batchSize) {
+        return list.stream().map(mapper);
+      }
+
+      final Executor limitingExecutor = new LimitingExecutor(parallelism);
+      final int batchCount = (list.size() - 1) / batchSize;
+      return IntStream.rangeClosed(0, batchCount)
+          .mapToObj(batch -> {
+            int startIndex = batch * batchSize;
+            int endIndex = (batch == batchCount) ? list.size() : (batch + 1) * batchSize;
+            return list.subList(startIndex, endIndex).stream();
+          })
+          .map(batch -> CompletableFuture.supplyAsync(() -> batch.map(mapper), limitingExecutor))
+          .flatMap(CompletableFuture::join);
+    });
+  }
+
+  private static class LimitingExecutor implements Executor {
+
+    private final Semaphore _limiter;
+
+    private LimitingExecutor(int maxParallelism) {
+      _limiter = new Semaphore(maxParallelism);
+    }
+
+    @Override
+    public void execute(Runnable command) {
+      try {
+        _limiter.acquire();
+        WORK_EXECUTOR.execute(command);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      } finally {
+        _limiter.release();
+      }
+    }
   }
 }
